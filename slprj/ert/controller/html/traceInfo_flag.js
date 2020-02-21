814856d35e04cbb5e1488edf1ae18ec9570b102e
Hello <logan.fillo@gmail.com>
2020-02-21 11:25:42 -0800

    working sim

--- slprj/ert/controller/html/traceInfo_flag.js
+++
@@ -1,9 +0,0 @@
-function TraceInfoFlag() {
-    this.traceFlag = new Array();
-}
-top.TraceInfoFlag.instance = new TraceInfoFlag();
-function TraceInfoLineFlag() {
-    this.lineTraceFlag = new Array();
-    this.lineTraceFlag["controller.cpp:29"]=1;
-}
-top.TraceInfoLineFlag.instance = new TraceInfoLineFlag();
--- slprj/ert/thrust_allocator/html/traceInfo_flag.js
+++
@@ -1,9 +0,0 @@
-function TraceInfoFlag() {
-    this.traceFlag = new Array();
-}
-top.TraceInfoFlag.instance = new TraceInfoFlag();
-function TraceInfoLineFlag() {
-    this.lineTraceFlag = new Array();
-    this.lineTraceFlag["thrust_allocator.cpp:27"]=1;
-}
-top.TraceInfoLineFlag.instance = new TraceInfoLineFlag();
