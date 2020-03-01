a16e8f5bb3965d74afb3a97f31610d454267a976
Hello <logan.fillo@gmail.com>
2020-02-29 22:09:34 -0800

    added real simulation, added real paramater values

--- Params/params20192020.m
+++ Params/params20192020.m
@@ -1,26 +1,26 @@
 % This script is run when this MATLAB project starts up
 
 % BLOCK PARAMETERS
-v = 1; % Desired averaged speed used by the trajectory planner (m/s)
+v = 0.1; % Desired averaged speed used by the trajectory planner (m/s)
 
 % Proportional gain matrix
-Kp_mat = [ 1, 0, 0, 0, 0, 0; ... % x pos err gain
-           0, 1, 0, 0, 0, 0; ... % y pos err gain
-           0, 0, 1, 0, 0, 0; ... % z pos err gain
-           0, 0, 0, 1, 0, 0; ... % phi rot err gain
-           0, 0, 0, 0, 1, 0; ... % theta rot err gain
-           0, 0, 0, 0, 0, 1 ];   % psi rot err gain
+Kp_mat = [ 7, 0, 0, 0, 0, 0; ... % x pos err gain
+           0, 10, 0, 0, 0, 0; ... % y pos err gain
+           0, 0, 7, 0, 0, 0; ... % z pos err gain
+           0, 0, 0, 7, 0, 0; ... % phi rot err gain
+           0, 0, 0, 0, 7, 0; ... % theta rot err gain
+           0, 0, 0, 0, 0, 5 ];   % psi rot err gain
        
 % Derivative gain matrix      
-Kd_mat = [ 1, 0, 0, 0, 0, 0; ... % x lin vel err gain
-           0, 1, 0, 0, 0, 0; ... % y lin vel err gain
-           0, 0, 1, 0, 0, 0; ... % z lin vel err gain
-           0, 0, 0, 1, 0, 0; ... % phi ang vel err gain
-           0, 0, 0, 0, 1, 0; ... % theta ang ve err gain
-           0, 0, 0, 0, 0, 1 ];   % psi ang ves err gain
+Kd_mat = [ 7, 0, 0, 0, 0, 0; ... % x lin vel err gain
+           0, 10, 0, 0, 0, 0; ... % y lin vel err gain
+           0, 0, 10, 0, 0, 0; ... % z lin vel err gain
+           0, 0, 0, 3, 0, 0; ... % phi ang vel err gain
+           0, 0, 0, 0, 3, 0; ... % theta ang ve err gain
+           0, 0, 0, 0, 0, 3 ];   % psi ang ves err gain
 
 % CONSTANTS
-const.rho_water = 1014.0; % water density          (Kg/m^2)
+const.rho_water = 998.0;  % water density          (Kg/m^2)
 const.g = 9.81;           % gravitational constant (m/s^2)
 
 % AUV MODEL PARAMETERS
@@ -28,8 +28,8 @@ const.g = 9.81;           % gravitational constant (m/s^2)
 % Parameters are stored in a single struct so it is easy to pass them into
 % a Simulink MATLAB function block
 
-params.m = 100;                               % mass of auv        (Kg)
-params.V = 100;                               % volume of auv      (m^3)
+params.m = 14.405;                            % mass of auv        (Kg)
+params.V = 0.0188;                            % volume of auv      (m^3)
 params.W = params.m*const.g;                  % auv weight         (Kg)
 params.B = const.rho_water*const.g*params.V;  % auv buoyancy force (N)
 
@@ -38,24 +38,24 @@ params.yg = 0;          % y coordinate centre of gravity (m)
 params.zg = 0;          % z coordinate centre of buoyancy (m)
 params.xb = 0;          % x coordinate centre of buoyancy (m)
 params.yb = 0;          % y coordinate centre of buoyancy (m)
-params.zb = 0;          % z coordinate centre of gravity (m)
-params.Ixx = 10;        % inertia around x-axis (Kg*m^2)
-params.Iyy = 10;        % inertia around y-axis (Kg*m^2)
-params.Izz = 10;        % inertia around z-axis (Kg*m^2)
+params.zb = 0.1;        % z coordinate centre of gravity (m)
+params.Ixx = 13.4;        % inertia around x-axis (Kg*m^2)
+params.Iyy = 15.7;        % inertia around y-axis (Kg*m^2)
+params.Izz = 8.98;        % inertia around z-axis (Kg*m^2)
 
-params.Xu = 10;         % x linear drag parameter (Ns/m)
-params.Yv = 10;         % y linear drag parameter (Ns/m)
-params.Zw = 10;         % z linear drag parameter (Ns/m)
-params.Kp = 10;         % roll linear drag parameter (Ns/m)
-params.Kq = 10;         % pitch linear drag parameter (Ns/m)
-params.Nr = 10;         % yaw linear drag parameter (Ns/m)
+params.Xu = 0.525;         % x linear drag parameter (Ns/m)
+params.Yv = 0;         % y linear drag parameter (Ns/m)
+params.Zw = 7.25;         % z linear drag parameter (Ns/m)
+params.Kp = 0;         % roll linear drag parameter (Ns/m)
+params.Kq = 0;         % pitch linear drag parameter (Ns/m)
+params.Nr = 0;         % yaw linear drag parameter (Ns/m)
 
-params.Xuu = 10;        % x quadratic drag parameter (Ns^2/m^2)
-params.Yvv = 10;        % y quadratic drag parameter (Ns^2/m^2)
-params.Zww = 10;        % z quadratic drag parameter (Ns^2/m^2)
-params.Kpp = 10;        % roll quadratic drag parameter (Ns^2/m^2)
-params.Kqq = 10;        % pitch quadratic drag parameter (Ns^2/m^2)
-params.Nrr = 10;        % yaw quadratic drag parameter  (Ns^2/m^2)
+params.Xuu = 85.625;        % x quadratic drag parameter (Ns^2/m^2)
+params.Yvv = 0;        % y quadratic drag parameter (Ns^2/m^2)
+params.Zww = 215;        % z quadratic drag parameter (Ns^2/m^2)
+params.Kpp = 0;        % roll quadratic drag parameter (Ns^2/m^2)
+params.Kqq = 0;        % pitch quadratic drag parameter (Ns^2/m^2)
+params.Nrr = 0;        % yaw quadratic drag parameter  (Ns^2/m^2)
 
 params.alpha = 45;  % angle of each horizontal thruster off 
                     % of the longitudinal axis (Deg)
